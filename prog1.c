1397ef6c97bd76a9fb46ec516fa7c792aa2d5a56
fuuuuuuli <s.kubo1031@gmail.com>
2025-01-21 13:09:26 +0900

    一旦完成

--- prog1.c
+++ prog1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "wavelib.h"
 
+int culculate_nagasa(int kaisu, int span_mode, double span, double span_decrease, int decrease_mode, double decrease, struct fmt fmt1, struct data data1);
+
 int main()
 {
     int kaisu, span_mode = 0, decrease_mode;
@@ -53,29 +55,68 @@ int main()
         }
     }
 
-    struct RIFF RIFF1; /* RIFFチャンク用構造体変数       */
-    struct fmt fmt1;   /* fmt チャンク用構造体変数       */
-    struct data data1; /* dataチャンク用構造体変数       */
-
-    int nagasa;         /* 連結後の音声の長さ             */
+    struct RIFF RIFF1;  /* RIFFチャンク用構造体変数       */
+    struct fmt fmt1;    /* fmt チャンク用構造体変数       */
+    struct data data1;  /* dataチャンク用構造体変数       */
+    int count = 0;      /* 制御変数                       */
+    int nagasa = 0;     /* 連結後の音声の長さ             */
+    double volume = 1;  /* 音量の倍率                     */
     unsigned char *oto; /* 連結後の音声を指すポインタ     */
 
-    for (int i = 0; i < kaisu; i++)
-    {
-        nagasa +=
-            if (span_mode == 1)
-        {
-        }
-        else if (span_mode == 2)
-        {
-        }
-    }
+    nagasa = culculate_nagasa(kaisu, span_mode, span, span_decrease, decrease_mode, decrease, fmt1, data1);
 
     load_wave_data(&RIFF1, &fmt1, &data1, "org1.wav");
 
-    struct RIFF RIFF2 = RIFF1;
-    struct RIFF RIFF3 = RIFF1;
-    int riff3_time;
+    oto = (unsigned char *)malloc(nagasa);
+    if (oto == NULL)
+    {
+        printf("メモリが確保できません．プログラムを終了します．\n");
+        exit(1);
+    }
+    else
+    {
+        for (int i = 0; i < data1.size_of_sounds; i++)
+        {
+            *(oto + count + i) = *(data1.sounds + i) * volume;
+        }
+        count += fmt1.BytesPerSec * span;
+        if (span_mode == 2)
+        {
+            span *= span_decrease;
+        }
+        if (decrease_mode == 1)
+        {
+            volume -= decrease;
+        }
+        else if (decrease_mode == 2)
+        {
+            volume *= decrease;
+        }
+    }
+
+    /* 音声１にコピー */
+    data1.size_of_sounds = nagasa;      /* 音声データサイズを修正   */
+    data1.sounds = oto;                 /* 音声のコピー             */
+    RIFF1.SIZE += data2.size_of_sounds; /* RIFFチャンクサイズの修正 */
+
+    /* wave ファイルの保存 */
+    printf("連結した音声を保存します．\n");
+    save_wave_data(&RIFF1, &fmt1, &data1, "out1.wav");
 
     return 0;
+}
+
+int culculate_nagasa(int kaisu, int span_mode, double span, double span_decrease, int decrease_mode, double decrease, struct fmt fmt1, struct data data1)
+{
+    int nagasa = 0;
+    for (int i = 0; i < kaisu; i++)
+    {
+        nagasa += fmt1.BytesPerSec * span;
+        if (span_mode == 2)
+        {
+            span *= span_decrease;
+        }
+    }
+    nagasa += data1.size_of_sounds;
+    return nagasa;
 }
